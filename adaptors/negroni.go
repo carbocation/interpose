aa939feed1d39a162be7a8902a6c589f8c03582a
James Pirruccello <james@carbocation.com>
2014-07-20 22:33:08 -0400

    Another small correction to the Negroni adapter to avoid over-aggressive teardown and reconstruction of the Negroni framework.

--- adaptors/negroni.go
+++ adaptors/negroni.go
@@ -8,19 +8,19 @@ import (
 
 func FromNegroni(handler negroni.Handler) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
+		n := negroni.New()
+		n.Use(handler)
+		n.UseHandler(next)
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			n := negroni.New()
-			n.Use(handler)
-			n.UseHandler(next)
 			n.ServeHTTP(rw, req)
 		})
 	}
 }
 
 func HandlerFromNegroni(handler negroni.Handler) http.Handler {
+	n := negroni.New()
+	n.Use(handler)
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		n := negroni.New()
-		n.Use(handler)
 		n.ServeHTTP(rw, req)
 	})
 }
