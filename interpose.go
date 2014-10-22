74a990880515d8cd8b854cfa2599ff813d016722
James Pirruccello <james@carbocation.com>
2014-10-22 12:36:41 -0400

    Updates the package description to accurately reflect FIFO.

--- interpose.go
+++ interpose.go
@@ -1,26 +1,22 @@
 /*
-Interpose is a minimalist Golang middleware that uses only http.Handler and
-func(http.Handler)http.Handler . Interpose takes advantage of closures to create
+Interpose is a minimalist Golang middleware that uses only `http.Handler` and
+`func(http.Handler)http.Handler`. Interpose takes advantage of closures to create
 a stack of native net/http middleware. Unlike other middleware libraries which
 create their own net/http-like signatures, interpose uses literal net/http
 signatures, thus minimizing package lock-in and maximizing inter-compatibility.
 
-Middleware is called in nested LIFO fashion, which means that the last middleware
+Middleware is called in nested FIFO fashion, which means that the first middleware
 to be added will be the first middleware to be called. Because the middleware is
-nested, it actually means that the last middleware to be added gets the
+nested, it actually means that the first middleware to be added gets the
 opportunity to make the first and the last calls in the stack. For example,
 if there are 3 middlewares added in order (0, 1, 2), the calls look like so:
 
-	//2 START
+	//0 START
 		//1 START
-			//0 START
-			//0 END
+			//2 START
+			//2 END
 		//1 END
-	//2 END
-
-Therefore, the last middleware generator to be added will not only be
-the first to be called, but will also have the opportunity to make the
-final call after the rest of the middleware is called
+	//0 END
 */
 package interpose
 
