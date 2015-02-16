50c09d12f8624ab10532f931cb630d0bf5f7c2c7
Carbocation <james@carbocation.com>
2015-02-15 20:31:35 -0500

    Updates the tests to add a benchmark demonstrating the difference between the "compiled" Interpose (i.e., calling .Handler() ) and the "raw" Interpose (i.e., not calling .Handler() ). NB, on my Macbook Air, the compiled Interpose is ~25% faster, requring 500 nanoseconds less time per request than the raw Interpose.

--- interpose_test.go
+++ interpose_test.go
@@ -1,50 +1,115 @@
 package interpose
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
 	"testing"
+
+	"github.com/interpose/middleware"
 )
 
-func TestServeHTTP(t *testing.T) {
-	result := ""
-	response := httptest.NewRecorder()
-
+func BasicMiddleware() *Middleware {
 	middle := New()
+
+	middle.Use(middleware.Json())
+	middle.Use(middleware.Buffer())
 
 	middle.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			result += "0"
+			fmt.Fprint(rw, "0")
 			next.ServeHTTP(rw, req)
-			result += "0"
+			fmt.Fprint(rw, "0")
 		})
 	})
 
 	middle.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			result += "1"
+			fmt.Fprint(rw, "1")
 			next.ServeHTTP(rw, req)
-			result += "1"
+			fmt.Fprint(rw, "1")
 		})
 	})
 
 	middle.UseHandler(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		result += "2"
+		fmt.Fprint(rw, "2")
 	}))
 
+	return middle
+}
+
+func TestCompiledMiddleware(t *testing.T) {
+	response := httptest.NewRecorder()
+
+	middle := BasicMiddleware().Handler()
+
 	middle.ServeHTTP(response, (*http.Request)(nil))
-	expect(t, result, "01210")
+	out, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	expect(t, string(out), "01210")
+}
+
+func TestServeHTTP(t *testing.T) {
+	response := httptest.NewRecorder()
+
+	middle := BasicMiddleware()
+
+	middle.ServeHTTP(response, (*http.Request)(nil))
+	out, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	expect(t, string(out), "01210")
 }
 
 func TestEmptyMiddleware(t *testing.T) {
-	result := ""
 	response := httptest.NewRecorder()
 
 	middle := New()
 
 	middle.ServeHTTP(response, (*http.Request)(nil))
-	expect(t, result, "")
+	out, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		t.Error(err)
+	}
+	expect(t, string(out), "")
+}
+
+func BenchmarkCompiled(b *testing.B) {
+	response := httptest.NewRecorder()
+
+	middle := BasicMiddleware().Handler()
+
+	for i := 0; i < b.N; i++ {
+		middle.ServeHTTP(response, (*http.Request)(nil))
+	}
+}
+
+func BenchmarkUncompiled(b *testing.B) {
+	response := httptest.NewRecorder()
+
+	middle := BasicMiddleware()
+
+	for i := 0; i < b.N; i++ {
+		middle.ServeHTTP(response, (*http.Request)(nil))
+	}
+}
+
+func BenchmarkEmpty(b *testing.B) {
+	response := httptest.NewRecorder()
+
+	middle := New()
+	middle.UseHandler(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+		return
+	}))
+
+	for i := 0; i < b.N; i++ {
+		middle.ServeHTTP(response, (*http.Request)(nil))
+	}
 }
 
 func expect(t *testing.T, a interface{}, b interface{}) {
